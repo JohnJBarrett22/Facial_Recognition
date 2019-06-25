18c44d45e191d89092b57399d0529bf61f5c22b0
John Barrett <johnjbarrett22@gmail.com>
2019-06-24 22:06:58 -0700

    Layered in pickle

--- faces-train.py
+++ faces-train.py
@@ -10,8 +10,8 @@ image_dir = os.path.join(BASE_DIR, 'images')
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-print(BASE_DIR)
-print(image_dir)
+# print(BASE_DIR)
+# print(image_dir)
 
 current_id = 0
 label_ids = {}
