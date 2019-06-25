7b10130a6fc5913db06eaed297acf1d2f58de93c
John Barrett <johnjbarrett22@gmail.com>
2019-06-24 17:06:06 -0700

    Added faces-train.py and several images to learn from, though I am having problems iterating through the files

---
+++ faces-train.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, 'images')
+
+print(BASE_DIR)
+print(image_dir)
+
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith('png') or file.endswith('jpg'):
+            path = os.path.join(root, file)
+            label = os.path.basename(os.path.dirname(path)).replace(' ', '-').lower()
+            print(label, path)
